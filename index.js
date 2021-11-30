cebc402156096cf95d9bc6e97a4e0289d707cd86
simonachess <simonachess@gmail.com>
2021-11-30 09:16:35 +0200

    initial

---
+++ index.js
@@ -0,0 +1,11 @@
+function compare(first, second) {
+    if (first === second) {
+        return console.log(true)
+    }
+    else {
+
+        return console.log(false)
+    }
+}
+
+compare(1, "1")
